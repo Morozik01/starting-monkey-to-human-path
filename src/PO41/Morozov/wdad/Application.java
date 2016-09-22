5df1eefd9affb0d4a39375ce983c173fc3fd598e
Morozik01 <morozik1333@gmail.com>
2016-09-22 11:10:46 +0400

    Added entry point

---
+++ src/PO41/Morozov/wdad/Application.java
@@ -0,0 +1,8 @@
+package PO41.Morozov.wdad;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.print("I’m Влад Морозов, and I’m not a monkey");
+    }
+}
